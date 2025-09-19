8a0e26cfc52aa4444d696260a8834d5107c96749
akagiyuu <khanghuynh0245@gmail.com>
2025-09-19 10:57:09 +0700

    feat(home): add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="flex min-h-svh flex-col items-center justify-center bg-black">
-            <Button variant="secondary">Click me</Button>
+        <div>
+            <div className="flex min-h-svh flex-col items-center justify-center bg-black text-white">
+                <div className="z-20 text-center space-y-5">
+                    <p className="font-bold text-6xl">MERLIN LABS</p>
+                    <p className="text-lg">Consulting | Marketing | Solution</p>
+                </div>
+                <Image
+                    className="absolute object-cover"
+                    src="/introduction-background.svg"
+                    alt="background"
+                    fill
+                    priority
+                />
+            </div>
         </div>
     );
 }
