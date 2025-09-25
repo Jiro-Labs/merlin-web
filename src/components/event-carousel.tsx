c43364b8dcc8271ebf9cc9f8386641cb6f56ae18
akagiyuu <khanghuynh0245@gmail.com>
2025-09-25 13:13:59 +0700

    fix: correct background linear gradient

--- src/components/event-carousel.tsx
+++ src/components/event-carousel.tsx
@@ -10,13 +10,19 @@ export const EventCard = ({ src, alt }: { src: string; alt: string }) => {
         <CarouselItem className="pl-2 sm:pl-3 md:pl-4 basis-4/5 sm:basis-1/2 lg:basis-1/3">
             <div className="w-full aspect-[4/3] relative rounded-lg overflow-hidden group">
                 <Image
-                    className="object-cover w-full h-full filter grayscale brightness-75 transition-all duration-300"
+                    className="object-contain w-full h-full filter grayscale brightness-75 transition-all duration-300"
                     src={src}
                     alt={alt}
                     fill
-                ></Image>
+                />
                 {/* Purple gradient overlay from bottom */}
-                <div className="h-2/5 absolute inset-0 top-auto bg-gradient-to-t from-purple-600/70 via-purple-500/30 to-transparent opacity-80 pointer-events-none" />
+                <div
+                    className="h-2/5 absolute inset-0 top-auto bottom-5 bg-gradient-to-t from-purple-600/70 via-purple-500/30 to-transparent opacity-80 pointer-events-none"
+                    style={{
+                        background:
+                            "linear-gradient(0.45deg, rgba(62,12,158,0.9) -20%, rgba(69,23,200,0.25), rgba(100,100,100,0) 70%)",
+                    }}
+                />
             </div>
         </CarouselItem>
     );
