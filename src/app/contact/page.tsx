e0feba332834a7144eee1440b81373607154daee
akagiyuu <khanghuynh0245@gmail.com>
2025-10-10 21:03:51 +0700

    feat: add contact form

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+export default function Contact() {
+    return (
+        <div>
+            <div>
+                <h1>
+                    Get in touch with us!
+                </h1>
+                <h2>
+                    If You Have Any Ideas, Please Fill Out The Form Below And Our Consultants Will Get In Touch With You Shortly.
+                </h2>
+            </div>
+            <div className="my-10 px-5 space-y-5">
+                <Input className="p-5 focus:border-violet-600 border-gray-500 hover:border-violet-600"
+                    placeholder="Your name"
+                />
+                <Input className="p-5 focus:border-violet-600 border-gray-500 hover:border-violet-600"
+                    placeholder="example@gmail.com"
+                    type="email"
+                />
+                <Input className="p-5 focus:border-violet-600 border-gray-500 hover:border-violet-600"
+                    placeholder="Your phone number"
+                    type="tel"
+                />
+                <Textarea
+                    className="p-5 focus:border-violet-600 border-gray-500 hover:border-violet-600"
+                    placeholder="Fill your text here_____"
+                    rows={15}
+                />
+                <div className="w-full">
+                    <Button className="
+                        block mx-auto px-6 py-2.5 bg-gradient-to-r from-violet-500 to-purple-600
+                        hover:from-violet-600 hover:to-purple-700 
+                        rounded-full transition-all duration-300 ease-in-out
+                        transform hover:scale-105 hover:shadow-lg hover:shadow-violet-500/25
+                        focus:outline-none focus:ring-2 focus:ring-violet-500/50
+                        text-sm font-medium">
+                        Connect via Email
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
