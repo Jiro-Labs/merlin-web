be0ecf71cb36439bfcd35c591e0a7af27eac3f1d
akagiyuu <khanghuynh0245@gmail.com>
2025-10-10 22:42:37 +0700

    fix: lint

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,4 @@
 import { Hexagon } from "@/components/hexagon";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
 
 export default function About() {
     return (
@@ -10,7 +7,9 @@ export default function About() {
                 <div className="space-y-5 max-w-3xl mx-auto text-white text-center">
                     <p className="text-5xl bold">About merlin labs</p>
                     <p className="text-xl">
-                        Merlin Labs specializes in developing IT solutions and comprehensive digital marketing strategies specifically designed for the Web3 ecosystem.
+                        Merlin Labs specializes in developing IT solutions and
+                        comprehensive digital marketing strategies specifically
+                        designed for the Web3 ecosystem.
                     </p>
                 </div>
             </div>
@@ -28,7 +27,11 @@ export default function About() {
                     </div>
                     <div>
                         <p className="text-2xl bold text-violet-700">VISION</p>
-                        <p>To be the core driving force and premier growth partner architecting the success of groundbreaking projects in the global Web3 ecosystem.</p>
+                        <p>
+                            To be the core driving force and premier growth
+                            partner architecting the success of groundbreaking
+                            projects in the global Web3 ecosystem.
+                        </p>
                     </div>
                 </div>
                 <div className="grid grid-cols-[auto_auto] gap-10">
@@ -40,7 +43,11 @@ export default function About() {
                     </div>
                     <div>
                         <p className="text-2xl bold text-violet-800">MISSION</p>
-                        <p>To provide comprehensive growth solutions for Web3 projects, combining technology, marketing, and a powerful network to ensure partner success.</p>
+                        <p>
+                            To provide comprehensive growth solutions for Web3
+                            projects, combining technology, marketing, and a
+                            powerful network to ensure partner success.
+                        </p>
                     </div>
                 </div>
                 <div className="grid grid-cols-[auto_auto] gap-10">
@@ -53,10 +60,22 @@ export default function About() {
                     <div>
                         <p className="text-2xl bold text-violet-900">VISION</p>
                         <ul className="px-6 list-disc space-y-3 my-3">
-                            <li>Proven Solutions: Delivering full-stack growth solutions with a strong track record.</li>
-                            <li>Powerful Network: Leveraging an extensive network of VCs and KOLs to amplify impact.</li>
-                            <li>Strategic Partnership: Our clients' success is our ultimate metric.</li>
-                            <li>Deep Expertise: An experienced team mastering the Web3 landscape.</li>
+                            <li>
+                                Proven Solutions: Delivering full-stack growth
+                                solutions with a strong track record.
+                            </li>
+                            <li>
+                                Powerful Network: Leveraging an extensive
+                                network of VCs and KOLs to amplify impact.
+                            </li>
+                            <li>
+                                Strategic Partnership: Our clients' success is
+                                our ultimate metric.
+                            </li>
+                            <li>
+                                Deep Expertise: An experienced team mastering
+                                the Web3 landscape.
+                            </li>
                         </ul>
                     </div>
                 </div>
--- src/components/hexagon.tsx
+++ src/components/hexagon.tsx
@@ -1,11 +1,16 @@
-export const Hexagon = ({
-    fill,
-}: {
-    fill: string,
-}) => {
+export const Hexagon = ({ fill }: { fill: string }) => {
     return (
-        <svg viewBox="0 0 102 89" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M25.5 0H76.5L102 44.4937L76.5 89H25.5L0 44.4937L25.5 0Z" fill={fill} />
+        <svg
+            role="img"
+            aria-label="hexagon"
+            viewBox="0 0 102 89"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M25.5 0H76.5L102 44.4937L76.5 89H25.5L0 44.4937L25.5 0Z"
+                fill={fill}
+            />
         </svg>
-    )
-}
+    );
+};
